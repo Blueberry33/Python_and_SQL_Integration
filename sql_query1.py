b6b0aedc427fc1fd2bbc055226efbfa1c61982e7
Blueberry33 <53601686+Blueberry33@users.noreply.github.com>
2021-04-24 14:38:24 +0530

    Add files via upload
    Q1: Write a query to find the doctors having more ratings than their supervisors.
    Employees ratings table
    | Employee Id |Employee Name |rating |supervisor employee Id |designation
    | 1 |Dr. Max | 9 | 3 |doctor
    | 2 |Dr. James | 8 | 4 |doctor
    | 3 | Peter | 6 | NULL |supervisor
    | 4 | Simon | 9 | NULL |supervisor
    For the above table, Dr.Max has more rating than his supervisor.

---
+++ sql_query1.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+"""
+#importing database
+import sqlite3;
+
+#connecting the databse to the program
+conn= sqlite3.connect('akaike_sql.db');
+print("connection established");
+cursor= conn.cursor();
+
+#dropping table so that previous versions are discarded
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEEE");
+
+#creating a table
+query= '''CREATE TABLE IF NOT EXISTS employeee (
+    `Employee_Id` INT NOT NULL,
+    `Employee_Name` VARCHAR(5),
+    `rating` INT,
+    `Supervisor_id` INT,
+    `Designation` VARCHAR(10) 
+)''';
+cursor.execute(query);
+conn.commit();
+print('Table created');
+
+#Adding values into the table
+query='''INSERT INTO employeee VALUES
+    (1,'Dr. Max',9,3,'doctor'),
+    (2,'Dr. James',8,4,'doctor'),
+    (3,'Peter',6,0,'Supervisor'),
+    (4,'Simon',9,0,'Supervisor');'''
+cursor.execute(query);
+conn.commit();
+print("Records inserted");
+   
+
+#Executing the query asked in the assignment
+query='''SELECT
+        firstt.Employee_Name 
+    FROM
+        `employeee` firstt 
+    INNER JOIN
+        `employeee` second 
+            ON firstt.Supervisor_id = second.Employee_id 
+    WHERE firstt.rating > second.rating;''';
+cursor.execute(query);
+result=cursor.fetchall();
+#Printing the results
+for x in range(len(result)):
+    print (result[x],end=' ');
+    print("earns more than his supervisor\n")
+conn.commit();
+
+#closing the connection
+conn.close();
